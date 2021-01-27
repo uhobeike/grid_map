73a5980929e1fb01720c463b9f01d99c29c4e03e
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-01-28 03:37:34 +0900

    Error avoidance

--- vi_grid_map_ros/src/image_publisher.cpp
+++ vi_grid_map_ros/src/image_publisher.cpp
@@ -7,6 +7,10 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <sys/stat.h>
+#include "unistd.h"
+#include <string.h>
+
 class ImagePublisher
 {
     //ros::NodeHandle nh_;
@@ -25,43 +29,56 @@ public:
         image_pub_ = pnh_.advertise<sensor_msgs::Image>(imageTopic_, 10);
 
         ros::Rate loop_rate(1);
+        
+        //ファイル存在チェック
+        struct stat st;
+        const char* file = imagePath_.c_str();
 
+        //error回避
+        sleep(10);
+        
         while(ros::ok()){
-            cv::Mat	image;
-            image = cv::imread(imagePath_);
+            int ret = stat(file, &st);
+            if (0 == ret){
+                cv::Mat	image;
+                image = cv::imread(imagePath_);
 
-            std::vector<int> param = std::vector<int>(2);
-            param[0] = CV_IMWRITE_PNG_COMPRESSION;
-            param[1] = 9;//default(3)  0-9.
+                std::vector<int> param = std::vector<int>(2);
+                param[0] = CV_IMWRITE_PNG_COMPRESSION;
+                param[1] = 9;//default(3)  0-9　最高設定
 
-            std::vector<uchar>buf;
-            cv::imencode(".png", image, buf, param);
-            cv::Mat im_decode = cv::imdecode(cv::Mat(buf), CV_LOAD_IMAGE_COLOR);
+                std::vector<uchar>buf;
+                cv::imencode(".png", image, buf, param);
+                cv::Mat im_decode = cv::imdecode(cv::Mat(buf), CV_LOAD_IMAGE_COLOR);
 
-            cv::Mat source = im_decode;
-            cv::Mat alpha_image = cv::Mat(source.size(), CV_8UC3);
-            cv::cvtColor(source, alpha_image, CV_RGB2RGBA);
-            
-            for (int y = 0; y < alpha_image.rows; ++y) {
-                for (int x = 0; x < alpha_image.cols; ++x) {
-                    cv::Vec4b px = alpha_image.at<cv::Vec4b>(x, y);
-                    if (px[0] + px[1] + px[2] == 0) {
-                        px[3] = 0;
-                        alpha_image.at<cv::Vec4b>(x, y) = px;
+                cv::Mat source = im_decode;
+                cv::Mat alpha_image = cv::Mat(source.size(), CV_8UC3);
+                cv::cvtColor(source, alpha_image, CV_RGB2RGBA);
+                
+                for (int y = 0; y < alpha_image.rows; ++y) {
+                    for (int x = 0; x < alpha_image.cols; ++x) {
+                        cv::Vec4b px = alpha_image.at<cv::Vec4b>(x, y);
+                        if (px[0] + px[1] + px[2] == 0) {
+                            px[3] = 0;
+                            alpha_image.at<cv::Vec4b>(x, y) = px;
+                        }
                     }
                 }
+                
+                std_msgs::Header header; // empty header
+                header.frame_id = "map_grid";
+                header.stamp = ros::Time::now();
+                
+                cv_bridge::CvImage img_bridge;
+                img_bridge = cv_bridge::CvImage(header, "bgra8", alpha_image);
+                
+                sensor_msgs::Image msg;
+                img_bridge.toImageMsg(msg);
+                image_pub_.publish(msg);
+            }   else    {    
+                std::string ros_err = imagePath_ + " Not Exist!";
+                ROS_ERROR("%s", ros_err.c_str());
             }
-            
-            std_msgs::Header header; // empty header
-            header.frame_id = "map_grid";
-            header.stamp = ros::Time::now();
-            
-            cv_bridge::CvImage img_bridge;
-            img_bridge = cv_bridge::CvImage(header, "bgra8", alpha_image);
-            
-            sensor_msgs::Image msg;
-            img_bridge.toImageMsg(msg);
-            image_pub_.publish(msg);
 
             ros::spinOnce();
             loop_rate.sleep();
